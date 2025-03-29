f46285dc9ada2a11cc08a7e35c50a4a6a140d615
Rugsit <rugsit.nest@gmail.com>
2025-03-29 09:45:41 +0700

    Resolve merge conflict

---
+++ frontend/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Login />
+</template>
+
+
+<script>
+import Login from "./login.vue"; // Import component
+
+export default {
+  components: {
+    Login, // Register component
+  },
+};
+</script>
---
+++ frontend/pages/login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex w-full h-screen justify-center items-center flex-col">
+    <p class=" text-[100px]">
+      Login
+    </p>
+    <input class=" border-gray-400 border-2 rounded-md outline-none p-4 w-full max-w-[500px]" />
+  </div>
+</template>
