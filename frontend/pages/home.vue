54ef64d77836ce7ab2bc4a47e8e4edb20408db97
Rugsit <rugsit.nest@gmail.com>
2025-03-29 11:19:43 +0700

    Create home page

---
+++ frontend/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class=" m-7">
+    <div class=" flex w-full justify-between items-start">
+      <div class="text-[50px]">
+        <p>Rank 1</p>
+        <p>Point 50</p>
+      </div>
+      <div>
+        <p class="text-[100px]">01:23:52:10</p>
+        <div class="max-w-[500px] w-full h-[300px] bg-gray-400 rounded-md">
+        </div>
+        <div class="flex justify-center">
+          <button
+            class=" bg-gray-400 rounded-md px-3 py-2 mt-[20px] w-[200px] hover:scale-90 transition-all cursor-pointer active:bg-gray-500">เข้าสู่เกม</button>
+        </div>
+      </div>
+      <p class="text-[50px]">User Name</p>
+    </div>
+  </div>
+</template>
